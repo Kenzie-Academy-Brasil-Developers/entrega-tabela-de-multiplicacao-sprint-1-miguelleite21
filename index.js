dfb86313cc3d58269ac7b1aa87dc375d826c0e26
Miguel Leite <miguelleite360@gmail.com>
2021-08-13 13:33:28 -0300

    bonus feito

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
  
-   function tabela() { // Substitua "nomeDaSuaFuncao" por um nome mais apropriado
+   function tabela(n) { // Substitua "nomeDaSuaFuncao" por um nome mais apropriado
     // TODO: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
-
+    
     let novaTabela = [];
-    for (let i = 0; i <= 10; i++) {
+    for (let i = 0; i <= n; i++) {
         novaTabela[i] = [];
-        for (let j = 0; j <= 10; j++) {
+        for (let j = 0; j <= n; j++) {
             novaTabela[i][j] = j*i
         }
     }
